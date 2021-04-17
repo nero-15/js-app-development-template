9d44d64450a5c4855b0ab6ad73f9396511191098
nero-15 <ti7805@gmail.com>
2021-04-17 23:05:05 +0900

    first commit

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+init();
+
+function init() {
+	console.log('hello world');
+}
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9qcy1hcHAtZGV2ZWxvcG1lbnQtdGVtcGxhdGUvLi9zcmMvYXBwLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7O0FBRUE7QUFDQTtBQUNBIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImluaXQoKTtcblxuZnVuY3Rpb24gaW5pdCgpIHtcblx0Y29uc29sZS5sb2coJ2hlbGxvIHdvcmxkJyk7XG59XG4iXSwic291cmNlUm9vdCI6IiJ9
